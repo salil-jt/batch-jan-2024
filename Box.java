d74dcb11a702b24cac785d505e1ab505615c2025
“Salil” <saliljavatrainings@gmail.com>
2024-02-09 21:45:52 +0530

    Adding volume and print methods - with no retunr type and no params

--- Box.java
+++ Box.java
@@ -2,60 +2,64 @@ class Box {
   double width;
   double height;
   double depth;
+  
+ // display volume of a box
+  void volume() {
+    System.out.println("Volume is ");
+    System.out.println(width * height * depth);
+
+  }
+  
+  void print(){
+  	System.out.println("Box width:"+width);
+	System.out.println("Box height:"+height);
+	System.out.println("Box depth:"+depth);	
+  
+  }
+ 
+  
+  
 }
 
 
 class BoxDemo {
   public static void main(String args[]) {
   
-  Box mybox = new Box();
-  System.out.println("In BoxDemo class");
+  	Box mybox = new Box();
+  	System.out.println("In BoxDemo class");
  	mybox.width=50;
 	mybox.height=10;
 	mybox.depth=20;
-	System.out.println("Box width:"+mybox.width);
-	System.out.println("Box height:"+mybox.height);
-	System.out.println("Box depth:"+mybox.depth);
-	System.out.println("Box volume:"+ (mybox.width*mybox.height*mybox.depth));
-	double vol = mybox.width*mybox.height*mybox.depth;
-	System.out.println("Box volume using variable:"+ vol);
+
+	//call the print method
+	mybox.print();
+
+	//call the volume method
 	
+	mybox.volume();
+	System.out.println("Execution done for mybox");
 	
+
 	Box mybox_1 = new Box();
 	mybox_1.width=15;
 	mybox_1.height=12;
 	mybox_1.depth=7;
-	System.out.println("Box_1 width:"+mybox_1.width);
-	System.out.println("Box_1 height:"+mybox_1.height);
-	System.out.println("Box_1 depth:"+mybox_1.depth);
-	System.out.println("Box_1 volume:"+ (mybox_1.width*mybox_1.height*mybox_1.depth));
+
+	mybox_1.print();
+	mybox_1.volume();
+
+
+
+	Box mybox_2 = new Box();
+	mybox_2.width=5;
+	mybox_2.height=2;
+	mybox_2.depth=8;
+	mybox_2.print();	
+
+	mybox_2.volume();
+
 	
-	/*  Assigning a reference to an object
-	Box b1 = new Box();
-	b1.height=15;
-	b1.width=12;	
-	b1.depth=5;	
-	
-	
-	Box b2 = b1;
-	System.out.println("B1 width: "+b1.width);
-	System.out.println("B2 depth: "+b2.depth);
-	
-	b2.depth = 2;
-	System.out.println("B2 depth after: "+b2.depth);
-	
-	System.out.println("B1 depth after: "+b1.depth);
-	
-	b1.depth = 1;
-	System.out.println("B2 depth after2: "+b2.depth);
-	
-	System.out.println("B1 depth after2: "+b1.depth);
-	
-	b1 = null;
-//	System.out.println("B1 depth after3: "+b1.depth); // null. -> NullPointerException
-	System.out.println("B2 depth after3: "+b2.depth);
-	
-	 */
+
 	
   
 }
