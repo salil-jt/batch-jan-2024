0f8f6d6a29b7a9b5109fe4fcf9352703d180006a
“Salil” <saliljavatrainings@gmail.com>
2024-02-09 23:01:26 +0530

    Default and Parameterized constructor

--- Box.java
+++ Box.java
@@ -3,6 +3,27 @@ class Box {
   double height;
   double depth;
   
+  
+  //Constructor
+  Box(){
+  System.out.println("In Box constructor");
+  width = 2;
+  height = 2;
+  depth = 2;
+  }
+  
+  
+  //Parameterized constructor
+  Box(double w, double h, double d){
+   System.out.println("In Box Parameterized constructor");
+    width = w;
+  	height = h;
+  	depth = d;
+  }
+  
+  
+  
+  
  // display volume of a box
   double volume() {
     System.out.println("Volume is "+(width * height * depth));
@@ -40,7 +61,7 @@ class BoxDemo {
   public static void main(String args[]) {
   
   
-  	Box mybox = new Box();
+  	Box mybox = new Box(50,10,20);
   	System.out.println("In BoxDemo class");
   	
   	//call print method before setting the values
@@ -48,7 +69,7 @@ class BoxDemo {
   	
 
 	//call setDim by passing in the width,height,depth
-	mybox.setDim(50,10,20);
+	//mybox.setDim(50,10,20); --use parameterized constructor
 
 
 	//call the volume method
@@ -58,7 +79,7 @@ class BoxDemo {
 	System.out.println("Execution done for mybox");
 	
 
-	Box mybox_1 = new Box();
+	Box mybox_1 = new Box(); //2,2,2
 	mybox_1.setDim(15,12,70);
 
 	//mybox_1.print();
