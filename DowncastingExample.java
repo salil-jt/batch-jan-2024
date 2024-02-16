4634302cb5674edf6d4aea6b599a2a529e0ad72a
“Salil” <saliljavatrainings@gmail.com>
2024-02-16 22:11:22 +0530

    Double to int

---
+++ DowncastingExample.java
@@ -0,0 +1,24 @@
+class DowncastingExample{
+public static void main(String arg[])
+    {
+    
+    
+    double area = 12.5f;
+    int areaNew = (float)area; //casting
+    System.out.println(areaNew);
+    
+    
+    int a = 10;
+    
+    int b = 12;
+    
+    int c = a;
+    
+    
+    c = b;
+    
+    
+    
+    }
+
+}
