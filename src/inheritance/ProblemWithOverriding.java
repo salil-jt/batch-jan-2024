f7554c7b118ccaf6d77d20486ac7910c92b83014
“Salil” <saliljavatrainings@gmail.com>
2024-02-20 00:11:08 +0530

    Final - in methods and classes

---
+++ src/inheritance/ProblemWithOverriding.java
@@ -0,0 +1,48 @@
+package inheritance;
+
+class Furniture{
+
+	void paint() {
+		
+		System.out.println("In paint of furniture");
+	}
+	
+	final void useWood() {
+		
+		System.out.println("Use only sandalwood");
+	}
+	//When we want to restrict or force that the sub class should not override the parent class method, we should use final keyword for the method
+
+
+	
+}
+class Table extends Furniture{
+	
+	@Override //Annotation
+	void paint() {
+		
+		System.out.println("In paint of Table");
+	}
+	/* This will give error
+void useWood() {
+		
+		System.out.println("Use cheap wood");
+	}
+	*/
+	
+	
+
+}
+
+
+
+
+
+
+public class ProblemWithOverriding {
+public static void main(String[] args) {
+	Table t  = new Table();
+	t.paint();
+	t.useWood();
+}
+}
---
+++ src/inheritance/ProblemWithOverridingClass.java
@@ -0,0 +1,22 @@
+package inheritance;
+
+final class Tree{//Parent class
+	
+	void noOfLeaves() {
+		
+		
+	}
+	//Marking a class final, makes it not suitable for extension/inheritance
+	
+}
+
+class BonsaiTree {//Sub class
+	
+	
+}
+
+
+
+public class ProblemWithOverridingClass {
+
+}
